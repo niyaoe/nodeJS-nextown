49ed4f924728f3b25e15cd08849fe21c778956fc
niyaoe <niya4334@gmail.com>
2026-01-27 15:28:47 +0530

    efd

--- Controller/testAPI.js
+++ Controller/testAPI.js
@@ -1,4 +1,7 @@
-const testAPI = () =>{
-    console.log("token Middleware is Working");
+const testAPI = (req,res) =>{
+    console.log("testAPI function is Working");
+    
+    res.status(203).json({message : "McLaren F1"})
+    res.end()
 }
 module.exports = {testAPI}
